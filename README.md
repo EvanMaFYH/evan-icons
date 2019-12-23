0bb219ad723a5db8d3db126067717b56ed6cc999
mayuefan <m13160102112@163.com>
2019-12-23 17:30:46 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # evan-icons
-uniapp扩展icon，基于font awesome
+uniapp icon扩展，基于[Font Awesome](http://www.fontawesome.com.cn/faicons/)，支持nvue模式，
+测试过微信小程序、app、app nvue模式、h5
+
+### 用法
+用法完全与[uni-icons](https://ext.dcloud.net.cn/plugin?id=28)一致，可参考[github demo](https://github.com/EvanMaFYH/evan-icons)中的用法，
+type值可以从[Font Awesome](http://www.fontawesome.com.cn/faicons/)网站获取
+### evan-icons props
+| 参数           | 说明            | 类型    | 可选值     | 默认值  |    
+| :------------- | :------------------------------ | :------ | :----- | :--- |  
+| type | 图标图案 | string | - | - |
+| size | 图标大小 | string/number | - | 16 |
+| color | 图标颜色 | string | - | #333333
+
+### evan-form events
+| 事件名   | 说明       | 回调参数     |   
+| :------- | :--------- | :-------|
+| click | 点击图标的回调 | - |
+
---
+++ pages/index/index.vue
@@ -0,0 +1,124 @@
+<template>
+	<view class="evan-icons-show">
+		<view class="evan-icons-show__block">
+			<text class="evan-icons-show__block__label">基本用法：</text>
+			<evan-icons type="glass"></evan-icons>
+			<evan-icons type="music"></evan-icons>
+			<evan-icons type="search"></evan-icons>
+			<evan-icons type="envelope-o"></evan-icons>
+		</view>
+		<view class="evan-icons-show__block">
+			<text class="evan-icons-show__block__label">自定义颜色：</text>
+			<evan-icons type="glass" color="#00ff00"></evan-icons>
+			<evan-icons type="music" color="#0000ff"></evan-icons>
+			<evan-icons type="search" color="#ff00ff"></evan-icons>
+			<evan-icons type="envelope-o" color="#ffff00"></evan-icons>
+		</view>
+		<view class="evan-icons-show__block">
+			<text class="evan-icons-show__block__label">自定义大小：</text>
+			<evan-icons type="glass" size="20"></evan-icons>
+			<evan-icons type="music" size="30"></evan-icons>
+			<evan-icons type="search" size="40"></evan-icons>
+			<evan-icons type="envelope-o" size="50"></evan-icons>
+		</view>
+		<view class="evan-icons-show__block">
+			<text class="evan-icons-show__block__label">点击事件：</text>
+			<evan-icons @click="onIconClick(1)" type="glass"></evan-icons>
+			<evan-icons @click="onIconClick(2)" type="music"></evan-icons>
+			<evan-icons @click="onIconClick(3)" type="search"></evan-icons>
+			<evan-icons @click="onIconClick(4)" type="envelope-o"></evan-icons>
+		</view>
+		<view class="evan-icons-show__block vertical">
+			<text class="evan-icons-show__block__label">全部图标：</text>
+			<view class="evan-icons-show__block__list">
+				<view class="evan-icons-show__block__list__item" v-for="(item,index) in iconArray" :key="index">
+					<evan-icons :type="item"></evan-icons>
+					<text class="evan-icons-show__block__list__item__text">{{item}}</text>
+				</view>
+			</view>
+		</view>
+		<!-- #ifdef APP-VUE -->
+		<button class="evan-icons-show__button" @click="goNvue">nvue页面</button>
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	import EvanIcons from '@/components/evan-icons/evan-icons.vue'
+	import icons from '@/components/evan-icons/icons.js'
+	export default {
+		components: {
+			EvanIcons
+		},
+		computed: {
+			iconArray() {
+				return Object.keys(icons)
+			}
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			onIconClick(index) {
+				uni.showToast({
+					title: `点击了第${index}个icon`,
+					icon: 'none'
+				})
+			},
+			goNvue() {
+				uni.navigateTo({
+					url: '../app/app'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.evan-icons-show {
+		&__button {
+			position: absolute;
+			right: 10px;
+			top: 10px;
+		}
+
+		&__block {
+			margin-bottom: 10px;
+
+			&.vertical {
+				display: flex;
+				flex-direction: column;
+			}
+
+			&__label {
+				font-size: 12px;
+				color: #333;
+			}
+
+			&__list {
+				display: flex;
+				flex-wrap: wrap;
+
+				&__item {
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					margin-top: 6px;
+					width: 25%;
+
+					&__text {
+						font-size: 12px;
+						color: #333;
+						margin-top: 6px;
+					}
+				}
+			}
+		}
+	}
+</style>
